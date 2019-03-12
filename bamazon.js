922448d8f50ed8adc70de39f6a851514653a3ac2
arinmsn <arinmsn@gmail.com>
2019-03-12 07:15:49 -0700

    App is functional.

--- bamazon.js
+++ bamazon.js
@@ -1,47 +1,47 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-require("dotenv").config();
-const chalk = require("chalk"); // 'chalk' ?
+var bamazonCustomer = require('./bamazonCustomer');
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+require('dotenv').config();
+const chalk = require('chalk'); // 'chalk' ?
 // const env = process.env;
 
-var pwd = require("./pwd"); // ./pwd.js
+var pwd = require('./pwd'); // ./pwd.js
 var sqlpwd = pwd.sqlpwd;
 
-var bamazonCustomer = require("./bamazonCustomer");
-
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
     port: 3306,
-    user: "root",
+    user: 'root',
     password: sqlpwd.password,
-    database: "bamazon"
+    database: 'bamazon'
 });
 
-connection.connect(function(error) {
-    if (error) throw error;
-    // console.log("connected as id " + connection.threadId);
-    inquirer.prompt([{
-        type: "list",
-        name: "levels",
-        message: "What would you like to do?",
-        choices: ["Customer","Manager","Supervisor","Exit"]
-    }]).then(answers => {
-        switch(answers.levels) { 
-        case "Customer":
-            bamazonCustomer();
-            break;
-        case "Manager":
-            console.log("Manager level is under construction!")
-            break;
-        case "Supervisor": 
-            console.log("Supervisor level is under construction!")
-        case "Exit":
-            connection.end();
-            break;
-        default:
-            console.log("You did not make a selection.")
-            break;
-        }
-    });
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.end();
 });
 
+inquirer.prompt([{
+    type: 'list',
+    name: 'levels',
+    message: 'What would you like to do?',
+    choices: ['Customer','Manager','Supervisor','Exit']
+}]).then(answers => {
+    switch(answers.levels) { 
+    case 'Customer':
+        runApp();
+        break;
+    case 'Manager':
+        console.log('Manager level is under construction!')
+        break;
+    case 'Supervisor': 
+        console.log('Supervisor level is under construction!')
+    case 'Exit':
+        connection.end();
+        break;
+    default:
+        console.log('You did not make a selection.')
+        break;
+    }
+});
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,12 +5,12 @@ const chalk = require("chalk"); // 'chalk' ?
 var Table = require("cli-table");
 // const env = process.env;
 
-module.exports = showInventory;
+// module.exports = runApp, showInventory, promptBuy;    
 
 var pwd = require("./pwd"); // ./pwd.js
 var sqlpwd = pwd.sqlpwd;
 
-var bamazon = require("./bamazon");
+// var bamazon = require("./bamazon");  
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -20,48 +20,137 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-var table = new Table({
-    head: ['Item ID', 'Name', 'Department', 'Price'],
-    colWidths: [10, 60, 20, 25]
+// function runApp() 
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`\nConnected as id ${connection.threadId}\n`)
+    showInventory();
 });
-// connection.connect(function(error) {
-//     if (error) throw error;
-//     showInventory();
-// });
+
+function showJustTable() {
+
+    connection.query('SELECT * FROM products', function(err, res) {
+        if (err) throw err;
+
+        var table = new Table({
+            head: ['Item ID', 'Name', 'Department', 'Price', 'Quantity'],
+            colWidths: [10, 60, 20, 25, 10]
+        });
+
+        for (var i=0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
+        } 
+        // console.log(`\n`)
+        console.log(table.toString());
+    });
+}
+
 
 function showInventory() { //displayItems()
     // console.log("Displaying all available items...");
-    var query = connection.query('SELECT * FROM products', function(err, res) {
-        if (err) {
-            console.log(err)
-        } 
-        
-        for (var i=0; i < res.length; i++) {
-            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price]);
-        } 
-        console.log('   ------------------------------- TESTING -------------------------------\n')
-        console.log(table.toString());
-        promptBuy();
-    });
+    
+    connection.query('SELECT * FROM products', function(err, res) {
+        if (err) throw err;
 
+        var table = new Table({
+            head: ['Item ID', 'Name', 'Department', 'Price', 'Quantity'],
+            colWidths: [10, 60, 20, 25, 10]
+        });
+
+        for (var i=0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
+        } 
+        // console.log(`\n`)
+        console.log(table.toString());
+
+        promptBuy();  
+    });
+    
 };  // End of showInventory()
+
+// validation_Function()
 
 function promptBuy() {
     inquirer.prompt([
     { 
-        name: "item_ID",
-        type: "input",
-        message: "What is the item ID of product you plan on purchasing",
-    },
+        type: 'input',
+        name: 'item_id',
+        message: 'Enter the item Id of product you plan on purchasing:  ',
+        // validate: validation__function()
+        filter: Number
+    }, 
     {
-        name: "quantity",
-        type: "input",
-        message: "How many of this product do you intend to order?"
+        type: 'input',
+        name: 'quantity',
+        message: 'Enter the quantity:  ',
+     // validate: validation__function() 
+        filter: Number
     }
-    ])
-    .then(function(answer) {
-        var query = "SELECT * FROM products WHERE ?";
+    ]).then(function(input) {
 
-    })
+        var item = input.item_id;
+        var quantity = input.quantity;
+        var queryBuyString = 'SELECT * FROM products WHERE ?';
 
-}   // End of promptBuy()
+        connection.query(queryBuyString, {item_id: item}, function (err, data) {
+            if (err) throw err;
+            
+            if (data.length === 0) {
+                console.log('Invalid item ID #. Please, enter a valid item ID: ');
+                showInventory();
+            } else { 
+                // if item is in stock ...
+                var itemData = data[0];
+                if (quantity <= itemData.stock_quantity) {
+                    console.log('Great news. It looks like we have that in stock!');
+                    console.log('Placing your order. Thank you for your patience.');
+                    var newQty = (itemData.stock_quantity - quantity);
+                
+                    var updatedQuery = 'UPDATE products SET ? WHERE ?'
+                    var total = (itemData.price * quantity).toFixed(2);
+
+                    // Updating inventory...
+                    var updatedQuery = 'UPDATE products SET stock_quantity = ' +
+                    (itemData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+
+                    connection.query(updatedQuery, function(err, res) {
+                        if (err) throw err;
+
+                        console.log(`\n`)
+                        console.log(`---------- Summary of your order ---------`);
+                        console.log(`Product: ${item}`);
+                        console.log(`Quantity: ${quantity}`);
+                        console.log(`------------------------------------------`);
+                        console.log(`Order total: $${total}`);
+                        console.log(`------------------------------------------`);
+                        console.log(`\n`);
+                        console.log('--- Inventory updated ---')
+                        showJustTable();
+                        connection.end();
+                    })
+                } else if (res[0].StockQuantity < quantity) {
+                    console.log('Insufficient product in stock.')
+                    console.log('Change your order, please.')
+                    console.log('\n');
+                    showInventory();
+
+                    // inquirer.prompt([
+                    //     {
+                    //         type: "confirm",
+                    //         name: "repeat",
+                    //         message: "Would you like to buy something else? "
+                    //     }
+                    // ]).then(function(input) {
+                    //     if (input) {
+                    //         promptBuy();
+                    //     } else {
+                    //         connection.end();
+                    //     }
+                    // });
+                }
+            } //else
+
+        });
+    });
+};
